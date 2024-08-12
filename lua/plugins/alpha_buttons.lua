34c8fe8e65ec2256a85ea34aa7c25276702e5f13
Krzysztof Piduch <krzysztof.piduch@gmail.com>
2024-08-12 18:25:57 +0200

    remove project mgr plugin

--- lua/plugins/alpha_buttons.lua
+++ lua/plugins/alpha_buttons.lua
@@ -1,9 +1,6 @@
 return {
   "goolord/alpha-nvim",
   opts = function(_, opts)
-    table.insert(opts.section.buttons.val, opts.button("SPC P", " Projects", ":ProjectMgr<CR>"))
-    -- opts.section.buttons.val = {
-    --   -- Buttons list
-    -- }
+    -- table.insert(opts.section.buttons.val, opts.button("LDR P  ", "  Projects", ":ProjectMgr<CR>"))
   end,
 }
